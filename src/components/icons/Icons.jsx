c4c9f83193831dc641b8dbb8b026d9d2b440bb86
keyan.namaghi <keyan.namaghi@sainsburys.co.uk>
2021-07-25 02:27:44 +0100

    :sparkles: added desktop icons

--- src/components/icons/Icons.jsx
+++ src/components/icons/Icons.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { SHOW_WINDOW } from '../../actions'
+
+import me from '../../assets/me.svg'
+import projects from '../../assets/projects.svg'
+import experience from '../../assets/experience.svg'
+import contact from '../../assets/contact.svg'
+
 import './Icons.css'
 
 export const Icons = () => {
@@ -16,21 +22,19 @@ export const Icons = () => {
   return (
     <div className="icon__container">
       <div className="icon" onClick={() => handleClick('/me')}>
-        <div className="icon__image">
-          <img className="icon__image--me" src="/me.svg" alt="" />
-        </div>
+        <img className="icon__image" src={me} alt="" />
         <span className="icon__name">Me</span>
       </div>
       <div className="icon" onClick={() => handleClick('/projects')}>
-        <div className="icon__image"></div>
+        <img className="icon__image" src={projects} alt="" />
         <span className="icon__name">Projects</span>
       </div>
       <div className="icon" onClick={() => handleClick('/experience')}>
-        <div className="icon__image"></div>
+        <img className="icon__image" src={experience} alt="" />
         <span className="icon__name">Experience</span>
       </div>
       <div className="icon" onClick={() => handleClick('/contact')}>
-        <div className="icon__image"></div>
+        <img className="icon__image" src={contact} alt="" />
         <span className="icon__name">Contact</span>
       </div>
     </div>
